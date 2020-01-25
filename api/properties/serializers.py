cbabb0c61bfaab1e0d424a76a3749b34f6339ea7
wanjala <gwiliez@gmail.com>
2020-01-25 11:15:11 +0300

    nest owner in serializer

--- api/properties/serializers.py
+++ api/properties/serializers.py
@@ -11,28 +11,33 @@ class PropertyTypeSerializer(serializers.ModelSerializer):
 
 
 class LandSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
     class Meta:
         model = Land
         fields = "__all__"
 
 
 class HostelSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
     class Meta:
         model = Hostel
         fields = "__all__"
 
 
 class StallSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
     class Meta:
         model = Stall
         fields = "__all__"
 
 class RentalSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
     class Meta:
         model = Rental
         fields = "__all__"
 
 class OfficeSpaceSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
     class Meta:
         model = OfficeSpace
         fields = "__all__"
