10a68ebbbddd62fa30c0e6cfbef4c737e02dc181
wanjala <gwiliez@gmail.com>
2020-01-28 19:25:18 +0300

    see if this fixed

--- frontend/server.js
+++ frontend/server.js
@@ -6,10 +6,12 @@ const app = express();
 
 const publicPath = path.join(__dirname, '..', 'frontend/build');
 
+console.log(publicPath)
+
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'build/index.html'));
+    res.sendFile(path.join(publicPath, 'index.html'));
  });
 
 app.listen(port, () => {
