2f8295d25bfe38f4e9c721ecf7e3ccb3ce9df4d3
wanjala <gwiliez@gmail.com>
2020-01-29 00:02:17 +0300

    see if this fixed

---
+++ frontend/build/precache-manifest.db88c323943d62e2d15ea40be2f17e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b23a5f4a1a7822679bf24939c5e16ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1343afd79bc24c617a",
+    "url": "/static/css/2.60af2688.chunk.css"
+  },
+  {
+    "revision": "0254e23f62145e35c7c7",
+    "url": "/static/css/main.99cd793f.chunk.css"
+  },
+  {
+    "revision": "1d1343afd79bc24c617a",
+    "url": "/static/js/2.9f65806f.chunk.js"
+  },
+  {
+    "revision": "801ad61d17028f25af5cbed9b1dfe504",
+    "url": "/static/js/2.9f65806f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0254e23f62145e35c7c7",
+    "url": "/static/js/main.9a18d2de.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);
