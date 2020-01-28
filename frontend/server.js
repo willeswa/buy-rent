9e1a5fcc2fed224706a27271f766b9010ef7b02c
wanjala <gwiliez@gmail.com>
2020-01-28 15:25:01 +0300

    fix purge

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,7 @@ const publicPath = path.join(__dirname, '..', 'frontend/build');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(publicPath, 'build/index.html'));
  });
 
 app.listen(port, () => {
