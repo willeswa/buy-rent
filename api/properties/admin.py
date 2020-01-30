b04ee6d454f68e991e57a528ef0f6aa8608d6efd
wanjala <gwiliez@gmail.com>
2020-01-30 23:12:09 +0300

    rm backend

--- api/properties/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import PropertyType, Land, Hostel, Stall, OfficeSpace
-
-admin.site.register(PropertyType)
-admin.site.register(Land)
-admin.site.register(Hostel)
-admin.site.register(Stall)
-admin.site.register(OfficeSpace)
-
